19fa50d4ebc6e7a9aecd310864a1fa840f6043a2
tyohanis-wolleli <tyohanis2018@gmail.com>
2021-03-18 14:19:11 +0000

    Tests added

---
+++ app/src/androidTest/java/com/example/my_application_one/SpinnerSelectTest.java
@@ -0,0 +1,76 @@
+package com.example.my_application_one;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+import android.content.Context;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.*;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.assertion.PositionAssertions.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class SpinnerSelectTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("com.example.my_application_one", appContext.getPackageName());
+    }
+
+    @Test
+    public void spinnerExists(){
+        onView(withId(R.id.spinner)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void spinnerAboveButton() {
+        onView(withId(R.id.spinner)).check(isCompletelyAbove(withId(R.id.button)));
+    }
+
+    @Test
+    public void spinnerSelectAndUpdate() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        String[] spinOpts = appContext.getResources().getStringArray(R.array.spinneroptions);
+
+        String firstItem =  spinOpts[0];
+
+        onView(withId(R.id.spinner)).perform(click());
+        onData(allOf(is(instanceOf(String.class)), is(firstItem))).perform(click());
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.textView)).check(matches(withText(firstItem)));
+    }
+}
--- app/src/main/java/com/example/my_application_one/MainActivity.java
+++ app/src/main/java/com/example/my_application_one/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.my_application_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClickUpdate(View view){
+        TextView currentgreeting = (TextView) findViewById(R.id.textView);
+        Spinner greetings = (Spinner) findViewById(R.id.spinner);
+        currentgreeting.setText(greetings.getSelectedItem().toString());
     }
 }
