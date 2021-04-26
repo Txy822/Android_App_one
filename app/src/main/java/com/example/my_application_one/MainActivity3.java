26ed09b4d35ac1bcf71f08a0ce79b7bbe36216c2
tyohanis-wolleli <tyohanis2018@gmail.com>
2021-04-26 14:57:35 +0100

    resources added

--- app/src/main/java/com/example/my_application_one/MainActivity3.java
+++ app/src/main/java/com/example/my_application_one/MainActivity3.java
@@ -3,6 +3,7 @@ package com.example.my_application_one;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class MainActivity3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
     }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+
 }
