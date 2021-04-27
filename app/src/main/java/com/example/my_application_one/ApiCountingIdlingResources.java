e92b3785a24f45e8ae87c0212278dfcef57b6b94
tyohanis-wolleli <tyohanis2018@gmail.com>
2021-04-28 00:08:42 +0100

    Idling resources class

---
+++ app/src/main/java/com/example/my_application_one/ApiCountingIdlingResources.java
@@ -0,0 +1,18 @@
+package com.example.my_application_one;
+
+import androidx.test.espresso.IdlingResource;
+import androidx.test.espresso.idling.CountingIdlingResource;
+
+public class ApiCountingIdlingResources {
+    private static CountingIdlingResource cIR = new CountingIdlingResource("api-call-idling-resource");
+
+    public static void increment() {
+        cIR.increment();
+    }
+    public static void decrement() {
+        cIR.decrement();
+    }
+    public static IdlingResource getIdlingResource() {
+        return cIR;
+    }
+}
